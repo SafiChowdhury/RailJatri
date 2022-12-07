762d2ab32a94a499801b5b1cdce8b2c03bb84215
Nazmul <nazmulislam4581@gmail.com>
2022-12-07 18:56:13 +0600

    up6

--- RailJatri/create_acc/models.py
+++ RailJatri/create_acc/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class passenger(models.Model):
+    first_name= models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    date_birth = models.DateField()
+    gender = models.CharField(max_length=15)
+    email = models.CharField(max_length=50, primary_key=True)
+    Nid_No=models.IntegerField()
+    address= models.CharField(max_length=200)
+    mobile_No= models.CharField(max_length=15)
+    password= models.CharField(max_length=50)
+    confirm_pass= models.CharField(max_length=50)
+
+
+
--- RailJatri/home/models.py
+++ RailJatri/home/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from create_acc.models import passenger
 
 # Create your models here.
+
+class login_details(models.Model):
+    email = models.ForeignKey(passenger, on_delete=models.CASCADE, related_name='sample3')
+    password = models.ForeignKey(passenger, on_delete=models.CASCADE, related_name='sample4')
